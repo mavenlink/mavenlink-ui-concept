3df25a4028eb402602c76343c7eb51b754027a2a
Andrew <ahuth@mavenlink.com>
2017-01-09 17:55:51 -0800

    Use `this` to set variables in tests
    Helps reduce test pollution, because the context of each test block
    gets reset in between each test.

--- src/molecules/brand-logo.test.js
+++ src/molecules/brand-logo.test.js
@@ -2,14 +2,12 @@ import React from "react"
 import {shallow} from "enzyme"
 import BrandLogo from "./brand-logo"
 
-describe("molecules <BrandLogo />", () => {
-  let component;
-
-  beforeEach(() => {
-    component = shallow(<BrandLogo />)
+describe("molecules <BrandLogo />", function () {
+  beforeEach(function () {
+    this.component = shallow(<BrandLogo />)
   })
 
-  it("has an M", () => {
-    expect(component.text()).toEqual("M")
+  it("has an M", function () {
+    expect(this.component.text()).toEqual("M")
   })
 })

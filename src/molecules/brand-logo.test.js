205e1c3296c982a34d9bef1ded24c8b4759ac0e2
Andrew <ahuth@mavenlink.com>
2017-01-09 17:49:01 -0800

    Update test namespace to include the directory structure

--- src/molecules/brand-logo.test.js
+++ src/molecules/brand-logo.test.js
@@ -2,7 +2,7 @@ import React from "react"
 import {shallow} from "enzyme"
 import BrandLogo from "./brand-logo"
 
-describe("<BrandLogo />", () => {
+describe("molecules <BrandLogo />", () => {
   let component;
 
   beforeEach(() => {

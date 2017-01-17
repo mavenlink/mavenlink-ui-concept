7bb7a76d0f391529845f56234a352d471f5a1281
Andrew <ahuth@mavenlink.com>
2017-01-16 21:36:39 -0800

    Add basic `documentation` loader that just gets the path from the import
    Tests currently broken because of this. It seems that when Jest runs,
    it is _not_ using Webpack, and therefore does not know about loaders.

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrandLogo } from '../src';
+// eslint-disable-next-line
+import documentation from './loaders/jsdoc!../src';
 
 export default function App() {
   return <BrandLogo size={200} />;

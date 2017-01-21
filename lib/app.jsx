0b25c365a468ecd9ed9b27c3cab840dad860736d
Andrew <ahuth@mavenlink.com>
2017-01-20 21:23:10 -0800

    Disable eslint for a line inline

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrandLogo } from '../src';
-// eslint-disable-next-line
-import documentation from './loaders/jsdoc!../src';
+import documentation from './loaders/jsdoc!../src'; // eslint-disable-line
 
 export default function App() {
   return <BrandLogo size={200} />;

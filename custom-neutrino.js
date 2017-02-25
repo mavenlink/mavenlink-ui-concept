bbd956eec243ed9a786ebbf973c31d09f10b9877
Andrew <andrew@huth.me>
2017-02-24 17:08:02 -0800

    Configure ESLint how I like it
    Unfortunately, Neutrino isn't reading the `.eslintrc.json`. Instead,
    for now, I have to setup it up through the custom Neutrino preset.

--- custom-neutrino.js
+++ custom-neutrino.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const merge = require('deepmerge');
 const path = require('path');
 const CWD = process.cwd();
 const SRC = path.join(CWD, 'src');
@@ -8,4 +9,21 @@ const LIB = path.join(CWD, 'lib');
 module.exports = neutrino => {
   // Configure Webpack to process our lib/ directory, in addition to src/.
   neutrino.config.module.rule('compile').include(SRC, LIB);
+
+  // Configure Neutrino to use our own ESLint rules.
+  neutrino.config.module.rule('lint').loader('eslint', ({options}) => {
+    return {
+      options: merge(options, {
+        rules: {
+          "comma-dangle": ["error", "never"],
+          "func-names": "off",
+          "import/no-extraneous-dependencies": "off",
+          "no-multiple-empty-lines": ["error", {"max": 1, "maxEOF": 1, "maxBOF": 0}],
+          "object-curly-spacing": ["error", "never"],
+          "quotes": ["error", "double", {"avoidEscape": true}],
+          "semi": ["error", "never"]
+        }
+      })
+    };
+  });
 };

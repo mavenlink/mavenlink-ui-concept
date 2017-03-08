224198e2f96f93e74f4c1e34db3c5c686b1ecd2e
Rob Levin <rob@mavenlink.com>
2017-03-08 14:43:52 -0800

    Further decompose Sections with individual Section items.

--- src/sections.jsx
+++ src/sections.jsx
@@ -1,14 +1,22 @@
 import React from "React"
 
+class Section extends React.Component {
+  render() {
+    return (
+      <li>
+        <h2 className="section-description">{this.props.component.description}</h2>
+        <span className="section-path">{this.props.component.path}</span>
+      </li>
+    )
+  }
+}
+
 class Sections extends React.Component {
   render() {
     return (
       <ul>
         {this.props.docs.map((component, i) =>
-          <li className="section" key={i}>
-            <h2 className="section-description">{component.description}</h2>
-            <span className="section-path">{component.path}</span>
-          </li>
+          <Section className="section" component={component} uniqueKey={i} key={i.toString()} />
         )}
       </ul>
     )

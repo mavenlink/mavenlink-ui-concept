683b1ca5ece98188a545cec6330cacebf919f0da
Rob Levin <rob@mavenlink.com>
2017-03-14 14:31:41 -0700

    Spiking on adding a theme for the prism code examples.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./app"
+import "./css/prism-fairyfloss.css"
 
 ReactDOM.render(React.createElement(App, null), document.getElementById("root"))

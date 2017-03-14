24433b94cadd7ef98668d3b5ad07d2eb5294a826
Rob Levin <rob@mavenlink.com>
2017-03-14 12:50:18 -0700

    Remove prism-react and do highlightAll my damn self :)

--- src/section.jsx
+++ src/section.jsx
@@ -1,15 +1,24 @@
 import React from "react"
+import Prism from "prismjs"
+import "prismjs/components/prism-javascript.js"
 
 class Section extends React.Component {
+  componentDidMount() {
+    Prism.highlightAll()
+  }
+  componentDidUpdate() {
+    Prism.highlightAll()
+  }
   render() {
-    const example = this.props.component.example
+    let example = this.props.component.example
+    example = example.replace(/ /g, "\u00a0").replace(/</g, "&lt;").replace(/>/g, "&gt;")
     return (
       <li>
         <h2 className="section-description">{this.props.component.description}</h2>
         <span className="section-path">{this.props.component.path}</span>
-        <div className="section-example">
-          {example.split("\n").map((item, key) => <div key={key}>{item}</div>)}
-        </div>
+        <code style={{whiteSpace: "pre"}} className="language-javascript section-example">
+          <div dangerouslySetInnerHTML={ {__html: example} } />
+        </code>
       </li>
     )
   }
